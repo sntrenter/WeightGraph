61a7b22db31d53347da7686ae543209776282349
Sam Trenter <sntrenter@gmail.com>
2024-10-07 14:53:13 -0500

    v.1

--- WeightCheck.py
+++ WeightCheck.py
@@ -8,6 +8,10 @@ import matplotlib.pyplot as plt
 import numpy as np
 intents = discord.Intents.default()  # Create a default intents object
 
+#TODO:add a test database
+#TODO: add a way to save a database
+
+
 # Add the specific intents your bot needs
 intents.message_content = True  # Allows reading message content
 #intents.message_reactions = True  # Allows reacting to messages
@@ -18,20 +22,29 @@ intents.message_content = True  # Allows reading message content
 bot = commands.Bot(command_prefix='!', intents=intents)
 
 def GraphWeight():
-
     conn = sqlite3.connect('weight.db')
     c = conn.cursor()
-    c.execute('''SELECT * FROM weights''')
+    c.execute('''SELECT * FROM weights ORDER BY Timestamp ASC;''')
     data = c.fetchall()
     conn.close()
     weights = [x[0] for x in data]
     times = [x[1] for x in data]
-    plt.plot(times, weights)
+
+    #fits (can consider doing more polynomial fits later)
+    for i in range(1,3):
+        coeff = np.polyfit(times, weights, i)
+        x = np.linspace(min(times), max(times), 100)
+        Polynomial = np.polyval(coeff, x)
+        plt.plot(x, Polynomial,color = f"C{i}",  label = f'polyfit of {i} degree')
+
+    plt.plot(times, weights , color = "C0" ,marker='x',label='weight')
     plt.xlabel('Time')
     plt.ylabel('Weight')
     plt.title('Weight vs Time')
+    plt.legend(loc='best')
+
     plt.savefig('weight.png')
-    
+    plt.close()
     return True
 
 @bot.event
@@ -40,13 +53,12 @@ async def on_ready():
 
 @bot.event
 async def on_message(message):
-    # Check if the message is from the bot itself to avoid reacting to its own messages.
     if message.author == bot.user:
         return
-    ## Check if the message was sent in the target channel
+
     target_channel = discord.utils.get(message.guild.channels, name="weightcheck")
     if message.channel == target_channel:
-        await message.add_reaction('🔄')#'✅')
+        await message.add_reaction('🔄')
 
         try:
             weight = float(message.content)
@@ -57,9 +69,11 @@ async def on_message(message):
             conn.commit()
             conn.close()
             await target_channel.send(f"Weight: {weight} \n Time: {current_time} \n Added to database.")
+            await message.remove_reaction('🔄', bot.user)
             await message.add_reaction('✅')
+            
 
-            if GraphWeight():
+            if GraphWeight(): #TODO: this is done in a weird way, needs a refactor
                 with open("weight.png", "rb") as f:
                     image = discord.File(f)
                 await target_channel.send(file = image)
@@ -70,6 +84,7 @@ async def on_message(message):
                 await target_channel.send("Error: Couldn't generate the graph.")
             
         except Exception as e:
+            await message.remove_reaction('🔄', bot.user)
             await message.add_reaction('❌')
             await target_channel.send(f"Error: {e}")
             
